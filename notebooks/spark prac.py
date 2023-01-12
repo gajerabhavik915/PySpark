31703b6dd8205dfb7e6013b2fa6610b0440d4ada
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-01-12 16:08:51 -0500

    Update: .gitignore Creating DataFrame.py README.md notebooks

--- notebooks/spark prac.py
+++ notebooks/spark prac.py
@@ -233,7 +233,7 @@ users = [{'id' : 1,
           'email': 'ghanu@shareehari.com',
           'is_customer': True,
           'amount_paid': 7000,
-          'Phone Number' : [23442677891, 25468136789],
+          'Phone Number' : None,
           'customer_from': 'Purushotam',
           'start_date': datetime.date(2020,4,1),
           'last_update': datetime.datetime(2021, 5,1,15,0)
@@ -289,8 +289,101 @@ from pyspark.sql.functions import explode
 
 users1. \
     withColumn('Phone Number', explode('Phone Number')). \
-    drop('Phone Number'). \
     show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+users1.select( 'id', col('Phone Number')[0].alias('Home Number'), col('Phone Number')[1].alias('Office Number')).show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import explode_outer 
+
+# COMMAND ----------
+
+users1. \
+    withColumn('Phone Number', explode_outer('Phone Number')). \
+    show()
+
+# COMMAND ----------
+
+users = [{'id' : 1,
+          'f_name':'Hari',
+          'l_name': 'Ghanu',
+          'email': 'ghanu@hari.com',
+          'is_customer': True,
+          'amount_paid': 4000,
+          'Phone Number' : {'home_number': '234567891', 'office_number':'2345136789'},
+          'customer_from': 'Akshardham',
+          'start_date': datetime.date(2020,1,1),
+          'last_update': datetime.datetime(2021, 1,1,15,0)
+         },
+         {
+          'id' : 2,
+          'f_name':'Ghanashyam',
+          'l_name': 'Ghanuji',
+          'email': 'ghanuji@ghanashyam.com',
+          'is_customer': False,
+          'amount_paid': 8000,
+          'Phone Number' : {'home_number':'234867891', 'office_number':'2346656789'},
+          'customer_from': 'Brahmhand',
+          'start_date': datetime.date(2020,2,1),
+          'last_update': datetime.datetime(2021, 2,1,15,0)
+         },
+         {
+          'id' : 3,
+          'f_name':'shreeHari',
+          'l_name': 'Ghanshyamji',
+          'email': 'ghanu@shareehari.com',
+          'is_customer': True,
+          'amount_paid': 7000,
+          'Phone Number' : None,
+          'customer_from': 'Purushotam',
+          'start_date': datetime.date(2020,4,1),
+          'last_update': datetime.datetime(2021, 5,1,15,0)
+         },
+         {
+             'id' : 4,
+          'f_name':'Lalaji',
+          'l_name': 'Lalacharan',
+          'email': 'ghanu@lalsharan.com',
+          'is_customer': False,
+          'amount_paid': 3000,
+          'Phone Number' : {'home_number':'345678915', 'office_number':'2345136789'},
+          'customer_from': 'AksharOradi',
+          'start_date': datetime.date(2020,8,1),
+          'last_update': datetime.datetime(2021,9,1,15,0)
+         }
+    
+]
+
+# COMMAND ----------
+
+user = spark.createDataFrame([Row(**user1) for user1 in users])
+
+# COMMAND ----------
+
+user.columns
+
+# COMMAND ----------
+
+user.dtypes
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+user.select( 'id', ('Phone Number')).show(truncate = False)
+
+# COMMAND ----------
+
+user.select('id', col('Phone Number')['home_number'].alias('Home_NUM'), col('Phone Number')['office_number'].alias('Office_NUM')).show()
 
 # COMMAND ----------
 
