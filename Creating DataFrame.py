c9ccaa73f703096cf6f958e8da87d786bfa35458
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-01-12 18:00:06 -0500

    Update Creating DataFrame.py

--- Creating DataFrame.py
+++ Creating DataFrame.py
@@ -1,18 +1,1 @@
-# Databricks notebook source
-spark
-
-# COMMAND ----------
-
-###Creating DataFrame in Spark
-
-# COMMAND ----------
-
-age = [10,11,14,13,24,16,45]
-
-# COMMAND ----------
-
-type(age)
-
-# COMMAND ----------
-
 
