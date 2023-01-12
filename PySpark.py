2390dc3d019d0e0f26cbc5f89642ce6c1a35feea
gajerabhavik915 <gajerabhavik915>
2023-01-12 20:16:45 +0000

    initial

---
+++ PySpark.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+spark
+
+# COMMAND ----------
+
+###Creating DataFrame in Spark
+
+# COMMAND ----------
+
+age = [10,11,14,13,24,16,45]
+
+# COMMAND ----------
+
+type(age)
+
+# COMMAND ----------
+
+
