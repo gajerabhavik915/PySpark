b1e111bbdeb352624037661de5c9616e10a51913
gajerabhavik915 <gajerabhavik915>
2023-01-13 03:23:15 +0000

    initial

--- Selecting and Renaming Columns.py
+++ Selecting and Renaming Columns.py
@@ -3,4 +3,68 @@ spark
 
 # COMMAND ----------
 
+import datetime
+
+# COMMAND ----------
+
+from pyspark.sql import Row
+
+# COMMAND ----------
+
+users = [{'id' : 1,
+          'f_name':'Hari',
+          'l_name': 'Ghanu',
+          'email': 'ghanu@hari.com',
+          'is_customer': True,
+          'amount_paid': 4000,
+          'Phone Number' : {'home_number': '234567891', 'office_number':'2345136789'},
+          'customer_from': 'Akshardham',
+          'start_date': datetime.date(2020,1,1),
+          'last_update': datetime.datetime(2021, 1,1,15,0)
+         },
+         {
+          'id' : 2,
+          'f_name':'Ghanashyam',
+          'l_name': 'Ghanuji',
+          'email': 'ghanuji@ghanashyam.com',
+          'is_customer': False,
+          'amount_paid': 8000,
+          'Phone Number' : {'home_number':'234867891', 'office_number':'2346656789'},
+          'customer_from': 'Brahmhand',
+          'start_date': datetime.date(2020,2,1),
+          'last_update': datetime.datetime(2021, 2,1,15,0)
+         },
+         {
+          'id' : 3,
+          'f_name':'shreeHari',
+          'l_name': 'Ghanshyamji',
+          'email': 'ghanu@shareehari.com',
+          'is_customer': True,
+          'amount_paid': 7000,
+          'Phone Number' : None,
+          'customer_from': 'Purushotam',
+          'start_date': datetime.date(2020,4,1),
+          'last_update': datetime.datetime(2021, 5,1,15,0)
+         },
+         {
+             'id' : 4,
+          'f_name':'Lalaji',
+          'l_name': 'Lalacharan',
+          'email': 'ghanu@lalsharan.com',
+          'is_customer': False,
+          'amount_paid': 3000,
+          'Phone Number' : {'home_number':'345678915', 'office_number':'2345136789'},
+          'customer_from': 'AksharOradi',
+          'start_date': datetime.date(2020,8,1),
+          'last_update': datetime.datetime(2021,9,1,15,0)
+         }
+    
+]
+
+# COMMAND ----------
+
+import pandas as pd
+
+# COMMAND ----------
+
 
