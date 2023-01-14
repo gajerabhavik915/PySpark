e78df317cb88c835f8f250038d0329530e29ffc6
gajerabhavik915 <gajerabhavik915>
2023-01-14 01:30:19 +0000

    initiate

--- Selecting and Renaming Columns.py
+++ Selecting and Renaming Columns.py
@@ -212,4 +212,128 @@ user.selectExpr('id', 'amount_paid', '(amount_paid + 25) AS New_Amt').show()
 
 # COMMAND ----------
 
+type(col('f_name'))
+
+# COMMAND ----------
+
+from pyspark.sql.functions import concat 
+
+# COMMAND ----------
+
+##Let's learn how to rename column by using 'withcolumn' 
+
+# COMMAND ----------
+
+user.select('id', concat(col('f_name'), lit(' '), col('l_name')).alias('Full_name')).show()
+
+# COMMAND ----------
+
+user.select('id', 'f_name', 'l_name').withColumn('Full_name', concat(col('f_name'), lit(' '), col('l_name')).alias('Fu_name')).show()
+
+# COMMAND ----------
+
+users = [{'id' : 1,
+          'f_name':'Hari',
+          'l_name': 'Ghanu',
+          'email': 'ghanu@hari.com',
+          'is_customer': True,
+          'courses' : [1,2],
+          'amount_paid': 4000,
+          'Phone Number' : Row(home_number='234567891', office_number='2345136789'),
+          'customer_from': 'Akshardham',
+          'start_date': datetime.date(2020,1,1),
+          'last_update': datetime.datetime(2021, 1,1,15,0)
+         },
+         {
+          'id' : 2,
+          'f_name':'Ghanashyam',
+          'l_name': 'Ghanuji',
+          'email': 'ghanuji@ghanashyam.com',
+          'is_customer': False,
+          'courses' : [1],
+          'amount_paid': 8000,
+          'Phone Number' : Row(home_number='234867891', office_number='2346656789'),
+          'customer_from': 'Bramhand',
+          'start_date': datetime.date(2020,2,1),
+          'last_update': datetime.datetime(2021, 2,1,15,0)
+         },
+         {
+          'id' : 3,
+          'f_name':'shreeHari',
+          'l_name': 'Ghanshyamji',
+          'email': 'ghanu@shareehari.com',
+          'is_customer': True,
+          'courses' : [1,2],
+          'amount_paid': 7000,
+          'Phone Number' : Row(home_number = None, office_number = None),
+          'customer_from': 'Purushotam',
+          'start_date': datetime.date(2020,4,1),
+          'last_update': datetime.datetime(2021, 5,1,15,0)
+         },
+         {
+             'id' : 4,
+          'f_name':'Lalaji',
+          'l_name': 'Lalacharan',
+          'email': 'ghanu@lalsharan.com',
+          'is_customer': False,
+          'courses' : [1],
+          'amount_paid': 3000,
+          'Phone Number' : Row(home_number='345678915', office_number='2345136789'),
+          'customer_from': 'AksharOradi',
+          'start_date': datetime.date(2020,8,1),
+          'last_update': datetime.datetime(2021,9,1,15,0)
+         }
+    
+]
+
+# COMMAND ----------
+
+user = spark.createDataFrame([Row(**user) for user in users])
+
+# COMMAND ----------
+
+user.select('id', 'courses').show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import size
+
+# COMMAND ----------
+
+user.select('id', 'courses').withColumn('CountOfCourses', size('courses')).show()
+
+# COMMAND ----------
+
+## we can renaming the column by using withColumnRenamed
+
+# COMMAND ----------
+
+user.select('id', 'f_name', 'l_name'). \
+   withColumnRenamed('id', 'usr_id'). \
+   withColumnRenamed('f_name', 'Fisrt_name').show()
+
+# COMMAND ----------
+
+user.select(user['id'].alias('user_id'), user['f_name'].alias('first_name'), user['l_name'].alias('last_name')). \
+   withColumn('Full_name', concat(col('first_name'), lit(' '), col('last_name'))). \
+   show()
+
+# COMMAND ----------
+
+required_column = ['id', 'f_name', 'l_name', 'email', 'is_customer']
+modified_column = ['usr_id', 'usr_f_name', 'usr_l_name', 'usr_email', 'usr_is_customer']
+## how to modified all column name togather
+
+# COMMAND ----------
+
+user.select(required_column).show()
+
+# COMMAND ----------
+
+user.select(required_column). \
+   toDF(*modified_column). \
+   show()
+
+# COMMAND ----------
+
 
