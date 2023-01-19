99e7cf561fc9619108d012d6820387d73f068778
gajerabhavik915 <gajerabhavik915>
2023-01-19 19:36:24 +0000

    initial

--- Aggregation on columns.py
+++ Aggregation on columns.py
@@ -3,4 +3,12 @@ spark
 
 # COMMAND ----------
 
+df1 = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/shared_uploads/c0851337@mylambton.ca/sample_data.csv")
+
+# COMMAND ----------
+
+df1.show()
+
+# COMMAND ----------
+
 
